c8f7e4f165d9c23636132298ae3282cd67d5fec1
smagulkassym <61790069+smagulkassym@users.noreply.github.com>
2023-04-15 12:29:55 +0600

    routes Creation

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 
 const app = express()
 
+app.use('/api', salesRouter);
+app.use('/api', suppliesRouter);
+app.use('/api', reportsRouter);
+
 app.listen(PORT, (error) => {
     error
         ? console.log(error)
