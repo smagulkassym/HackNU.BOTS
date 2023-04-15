7593d129630d2ff573a5782c5308d0ccf6dd9407
smagulkassym <61790069+smagulkassym@users.noreply.github.com>
2023-04-16 02:03:50 +0600

    Script Update

--- PythonScripts/test.py
+++ PythonScripts/test.py
@@ -1,82 +1,63 @@
 from collections import defaultdict
 from datetime import datetime, timedelta
 import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+import time
 
-# Read the supply and sale tables into pandas dataframes
-supply_df = pd.read_csv('umaghacknu_supply.csv', header=None)
-sale_df = pd.read_csv('umaghacknu_sale.csv', header=None)
+start_time = time.time()
 
-supply_df.columns = ["id", "barcode", "quantity", "price", "supplyTime"]
-sale_df.columns = ["id", "barcode", "quantity", "price", "saleTime"]
+engine = create_engine("postgresql+psycopg2://postgres:Ayan2002@localhost/umag_hacknu")
 
-print(supply_df.columns)
-print(sale_df.columns)
 
-def compute_net_profit(supply_df, sale_df, start_time, end_time):
+def calculate_gross_profit(barcode, fromTime, toTime):
+    sql_querySales = f"SELECT barcode, price, quantity, sale_time FROM umag_hacknu.sale WHERE barcode = {barcode} AND sale_time >= timestamp '{fromTime}' AND sale_time <= timestamp '{toTime}';"
+    sql_querySupplies = f"SELECT barcode, price, quantity, supply_time FROM umag_hacknu.supply WHERE barcode = {barcode} AND supply_time <= timestamp '{toTime}';"
+
+
+    supply_df = pd.read_sql_query(sql_querySupplies, engine)
+    sale_df = pd.read_sql_query(sql_querySales, engine)
+    # Filter sales and supplies dataframes based on the given parameters
+    # Sort the filtered supplies dataframe by supply_time
+    # supply_df = supply_df.sort_values(by='supply_time')
+
+    # Initialize variables
+    total_quantity = 0
+    total_revenue = 0
+    total_cost = 0
+
+    # Loop through the filtered sales dataframe and calculate the gross profit using the FIFO approach
+    for index, sale in sale_df.iterrows():
+        sale_quantity = sale['quantity']
+        sale_price = sale['price']
+        total_quantity += sale_quantity
+        total_revenue += sale_quantity * sale_price
+
+        while sale_quantity > 0:
+            supply = supply_df.iloc[0]
+            supply_quantity = supply['quantity']
+            supply_price = supply['price']
+
+            if sale_quantity >= supply_quantity:
+                total_cost += supply_quantity * supply_price
+                sale_quantity -= supply_quantity
+                supply_df.iloc[0, 1] = 0
+                supply_df = supply_df[supply_df['quantity'] > 0]
+            else:
+                total_cost += sale_quantity * supply_price
+                supply_df.iloc[0, 1] -= sale_quantity
+                sale_quantity = 0
+
+    # Calculate the gross profit
+    gross_profit = total_revenue - total_cost
+
+    # return result
+    return barcode, total_quantity, total_revenue, gross_profit
     
-    # Filter the supply and sale dataframes by the given time period
-    supply_period_df = supply_df[(supply_df['supplyTime'] >= start_time) & (supply_df['supplyTime'] <= end_time)]
-    sale_period_df = sale_df[(sale_df['saleTime'] >= start_time) & (sale_df['saleTime'] <= end_time)]
-    
-    # Sort the dataframes by time
-    supply_period_df = supply_period_df.sort_values('supplyTime')
-    sale_period_df = sale_period_df.sort_values('saleTime')
-    
-    # Create a dictionary to keep track of remaining quantity and purchase prices for each barcode
-    supply_dict = defaultdict(list)
-    for row in supply_period_df.itertuples():
-        supply_dict[row.barcode].append((row.quantity, row.price, row.supplyTime))
-    
-    # Iterate through the sale table and calculate profits
-    profits_data = []
-    net_profit = 0
-    for sale_row in sale_period_df.itertuples():
-        barcode = sale_row.barcode
-        sale_quantity = sale_row.quantity
-        sale_price = sale_row.price
-        sale_time = sale_row.saleTime
-        
-        if barcode not in supply_dict:
-            continue
-        
-        total_supply_quantity = 0
-        oldest_purchase_price = 0
-        for i, (supply_quantity, purchase_price, purchase_time) in enumerate(supply_dict[barcode]):
-            total_supply_quantity += supply_quantity
-            if total_supply_quantity >= sale_quantity:
-                oldest_purchase_price = purchase_price
-                break
-        
-        if total_supply_quantity < sale_quantity:
-            COGS = 0
-        else:
-            COGS = sale_quantity * oldest_purchase_price
-        
-        gross_profit = sale_price * sale_quantity - COGS
-        net_profit += gross_profit
-        
-        # Update the remaining quantity for the oldest supply operation
-        if total_supply_quantity == sale_quantity:
-            del supply_dict[barcode][i]
-        elif total_supply_quantity > sale_quantity:
-            supply_dict[barcode][i] = (supply_quantity - sale_quantity, purchase_price, purchase_time)
-        else:
-            while total_supply_quantity < sale_quantity and supply_dict[barcode] :
-                supply_quantity, purchase_price, purchase_time = supply_dict[barcode].pop(0) 
-                total_supply_quantity += supply_quantity
-                COGS += supply_quantity * purchase_price
-            if total_supply_quantity > sale_quantity:
-                supply_dict[barcode].insert(0, (total_supply_quantity - sale_quantity, purchase_price, purchase_time))
-        
-        profits_data.append((barcode, sale_price, purchase_price, sale_quantity,  COGS, gross_profit, sale_time))
-    profits_df = pd.DataFrame(profits_data, columns=['barcode', 'salePrice','purchasePrice','quantity','GOCS' ,'gross_profit', 'sale_time'])
-    profits_df.to_csv('profits.csv', index=False)
-    return net_profit
+   
+print(calculate_gross_profit('48743587','2022-01-01 00:00:00', '2023-01-04 15:45:00'))
 
+end_time = time.time()
+time_taken = end_time - start_time
 
-
-
-# Create a dictionary to keep track of remaining quantity and purchase prices for each barcode
-# Create a profits table
-print(compute_net_profit(supply_df, sale_df, '2022-03-01 12:00:00', '2022-05-01 12:00:00'))
-
+print(f"Time taken: {time_taken:.5f} seconds")
